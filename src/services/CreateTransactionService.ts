98b57db2dd4b17120b570429548f5bcada1f66d5
Marcelo Rondelli Brito <marcelo.brito@decision-one.net>
2020-08-29 15:51:18 -0300

    feito

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,11 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +13,27 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    const { total } = this.transactionsRepository.getBalance();
+
+    // ---Verifica se possui saldo suficiente para a operacao, quando saida
+    if (type === 'outcome' && value > total) {
+      throw Error('Insufficient balance');
+    }
+
+    // ---Verifica se o tipo de transacao é um valor válido
+    if (!['income', 'outcome'].includes(type)) {
+      throw Error('The type should be income or outcome');
+    }
+
+    // ---Chama o metodo do repositorio para efetivar a persistencia
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
